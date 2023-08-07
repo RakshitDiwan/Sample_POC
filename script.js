7a3495442b8abe2ba55460df7458b3b710228740
Rakshit Diwan <rakshit.diwan@hcl.com>
2023-08-07 14:35:58 +0530

    I am commiting the changes made to all the files

---
+++ script.js
@@ -0,0 +1,29 @@
+let x = document.getElementsByClassName('container')
+console.log(x)
+x[0].style.background = "yellow";
+
+x[0].classList.add("background")
+
+z = document.createElement('p');
+z.innerText="I am learning javascript"
+x[0].appendChild(z)
+
+z2 = document.createElement('p');
+z2.innerText = "Hello I am learning different javascript";
+
+// x[0].replaceChild(z2, z);
+// Once this line will be executed, the text will be changed.
+let btn = document.getElementById("button")
+const body=document.body;
+btn.addEventListener('click', () =>{
+    body.style.backgroundColor=getRandomColor()
+});
+
+function getRandomColor(){
+    const letters=[0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+    let color = '#';
+    for(let i =0; i<6; i++){
+        color+=letters[Math.floor(Math.random()*16)];
+    }
+    return color;
+}
