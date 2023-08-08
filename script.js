ed2d1c4216509474694b08c461209eb0cbcdd19c
Rakshit Diwan <rakshit.diwan@hcl.com>
2023-08-09 00:39:11 +0530

    Commiting third time

--- script.js
+++ script.js
@@ -13,12 +13,11 @@ z2.innerText = "Hello I am learning different javascript";
 
 // x[0].replaceChild(z2, z);
 // Once this line will be executed, the text will be changed.
+
+// this code will change the color of the page everytime the click me button is clicked after 2 seconds
 let btn = document.getElementById("button")
 const body=document.body;
-btn.addEventListener('click', () =>{
-    body.style.backgroundColor=getRandomColor()
-});
-
+btn.addEventListener('click', () =>{setTimeout(() => {body.style.background = getRandomColor();}, 2000)});
 function getRandomColor(){
     const letters=[0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
     let color = '#';
